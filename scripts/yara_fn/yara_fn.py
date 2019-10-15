186df73b97a213ad4210f8464de88dc350af8508
tylerha97 <tylerha97@gmail.com>
2019-10-15 15:30:52 -0400

    yara_fn to ida 7.4 and python 3

--- scripts/yara_fn/yara_fn.py
+++ scripts/yara_fn/yara_fn.py
@@ -37,8 +37,8 @@ def get_basic_blocks(fva):
         return ret
 
     for bb in idaapi.FlowChart(func):
-        ret.append(BasicBlock(va=bb.startEA,
-                              size=bb.endEA - bb.startEA))
+        ret.append(BasicBlock(va=bb.start_ea,
+                              size=bb.end_ea - bb.start_ea))
 
     return ret
 
@@ -47,7 +47,7 @@ def get_function(va):
     '''
     return va for first instruction in function that contains given va.
     '''
-    return idaapi.get_func(va).startEA
+    return idaapi.get_func(va).start_ea
 
 
 Rule = namedtuple('Rule', ['name', 'bytes', 'masked_bytes'])
@@ -57,7 +57,7 @@ def is_jump(va):
     '''
     return True if the instruction at the given address appears to be a jump.
     '''
-    return idc.GetMnem(va).startswith('j')
+    return idc.print_insn_mnem(va).startswith('j')
 
 
 def get_basic_block_rule(bb):
@@ -71,7 +71,7 @@ def get_basic_block_rule(bb):
     va = bb.va
     while va < bb.va + bb.size:
         insns.append(va)
-        va = idc.NextHead(va)
+        va = idc.next_head(va)
 
     # drop the last instruction if its a jump
     if is_jump(insns[-1]):
@@ -82,7 +82,7 @@ def get_basic_block_rule(bb):
     #   not yet join'd for performance.
     masked_bytes = []
     for va in insns:
-        size = idc.ItemSize(va)
+        size = idc.get_item_size(va)
         if idaapi.contains_fixups(va, size):
             # fetch the fixup locations within this one instruction.
             fixups = []
@@ -106,22 +106,22 @@ def get_basic_block_rule(bb):
             
             # fetch and format each byte of the instruction,
             #  possibly masking it into an unknown byte if its a fixup.
-            for i, byte in enumerate(idc.GetManyBytes(va, size)):
+            for i, byte in enumerate(idc.get_bytes(va, size)):
                 byte_addr = i + va
                 if byte_addr in fixup_byte_addrs:
-                    bytes.append(ord(byte))
+                    bytes.append(byte)
                     masked_bytes.append('??')
                 else:
-                    bytes.append(ord(byte))
-                    masked_bytes.append('%02X' % (ord(byte)))
-        elif 'call' in idc.GetMnem(va):
-            for i, byte in enumerate(idc.GetManyBytes(va, size)):
-                bytes.append(ord(byte))
+                    bytes.append(byte)
+                    masked_bytes.append('%02X' % (byte))
+        elif 'call' in idc.print_insn_mnem(va):
+            for i, byte in enumerate(idc.get_bytes(va, size)):
+                bytes.append(byte)
                 masked_bytes.append('??')
         else:
-            for byte in idc.GetManyBytes(va, size):
-                bytes.append(ord(byte))
-                masked_bytes.append('%02X' % (ord(byte)))
+            for byte in idc.get_bytes(va, size):
+                bytes.append(byte)
+                masked_bytes.append('%02X' % (byte))
 
     return Rule('$0x%x' % (bb.va), bytes, masked_bytes)
 
@@ -132,7 +132,7 @@ def format_rules(fva, rules):
      the function, format a complete YARA rule that matches all of the
      basic block signatures.
     '''
-    name = idc.GetFunctionName(fva)
+    name = idc.get_func_name(fva)
 
     # some characters aren't valid for YARA rule names
     safe_name = name
@@ -142,19 +142,15 @@ def format_rules(fva, rules):
 
     md5 = idautils.GetInputFileMD5()
     ret = []
-    ret.append('rule a_{hash:s}_{name:s} {{'.format(
-        hash=md5,
-        name=safe_name))
+    ret.append(f'rule a_{md5}_{safe_name}')
     ret.append('  meta:')
-    ret.append('    sample_md5 = "{md5:s}"'.format(md5=md5))
-    ret.append('    function_address = "0x{fva:x}"'.format(fva=fva))
-    ret.append('    function_name = "{name:s}"'.format(name=name))
+    ret.append(f'    sample_md5 = "{md5}"')
+    ret.append(f'    function_address = "0x{fva}"')
+    ret.append(f'    function_name = "{name}"')
     ret.append('  strings:')
     for rule in rules:
         formatted_rule = ' '.join(rule.masked_bytes)
-        ret.append('    {name:s} = {{ {hex:s} }}'.format(
-            name=rule.name,
-            hex=formatted_rule))
+        ret.append(f'    {rule.name} = {formatted_rule}')
     ret.append('  condition:')
     ret.append('    all of them')
     ret.append('}')
@@ -173,7 +169,7 @@ def create_yara_rule_for_function(fva):
         # ensure there at least MIN_BB_BYTE_COUNT
         #  non-masked bytes in the rule, or ignore it.
         # this will reduce the incidence of many very small matches.
-        unmasked_count = len(filter(lambda b: b != '??', rule.masked_bytes))
+        unmasked_count = len([b for b in rule.masked_bytes if b != '??'])
         if unmasked_count < MIN_BB_BYTE_COUNT:
             continue
 
@@ -187,11 +183,11 @@ def get_segment_buffer(segstart):
     fetch the bytes of the section that starts at the given address.
     if the entire section cannot be accessed, try smaller regions until it works.
     '''
-    segend = idaapi.getseg(segstart).endEA
+    segend = idaapi.getseg(segstart).end_ea
     buf = None
     segsize = segend - segstart
     while buf is None:
-        buf = idc.GetManyBytes(segstart, segsize)
+        buf = idc.get_bytes(segstart, segsize)
         if buf is None:
             segsize -= 0x1000
     return buf
@@ -205,7 +201,7 @@ def get_segments():
     fetch the segments in the current executable.
     '''
     for segstart in idautils.Segments():
-         segend = idaapi.getseg(segstart).endEA
+         segend = idaapi.getseg(segstart).end_ea
          segsize = segend - segstart
          segname = str(idc.SegName(segstart)).rstrip('\x00')
          segbuf = get_segment_buffer(segstart)
@@ -233,15 +229,15 @@ def test_yara_rule(rule):
     for segment in get_segments():
         matches = r.match(data=segment.buf)
         if len(matches) > 0:
-            logger.info('generated rule matches section: {:s}'.format(segment.name))
+            logger.info('generated rule matches section: {segment.name}')
             return True
     return False
 
 
 def main():
-    va = idc.ScreenEA()
+    va = idc.get_screen_ea()
     fva = get_function(va)
-    print('-' * 80)
+    print(('-' * 80))
     rule = create_yara_rule_for_function(fva)
     print(rule)
 
