a453a26c2a7094eabaac269e45ee14c506ee7e76
0xebfehat <26025118+0xebfehat@users.noreply.github.com>
2020-06-25 16:25:56 +0900

    Add files via upload
    Switched to IDA 7.x API

--- scripts/parse_uuid.py
+++ scripts/parse_uuid.py
@@ -4,6 +4,6 @@ import ida_bytes
 import ida_kernwin
 
 
-buf = ida_bytes.get_many_bytes(ida_kernwin.get_screen_ea(), 0x10)
+buf = ida_bytes.get_bytes(ida_kernwin.get_screen_ea(), 0x10)
 uid = uuid.UUID(bytes_le=buf)
 print(uid)
