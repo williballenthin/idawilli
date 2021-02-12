8abf6d447a3bb9060862add83910d3ba8ad83712
Willi Ballenthin <william.ballenthin@fireeye.com>
2021-02-12 14:35:44 -0700

    go_inline_strings: use right src code

--- scripts/go/go_fixup_inline_strings.py
+++ scripts/go/go_fixup_inline_strings.py
@@ -8,6 +8,7 @@ expect to see the assembly pattern:
   mov [stack], $size
 """
 import idc
+import ida_ua
 import ida_name
 import ida_bytes
 import idautils
@@ -23,11 +24,10 @@ def enum_segments():
 def find_pointers(start, end):
     for va in range(start, end-0x8):
         ptr = ida_bytes.get_qword(va)
-        if idc.get_segm_start(ptr) != idc.BADADDR:
-            yield va, ptr, 8
-        ptr = ida_bytes.get_dword(va)
-        if idc.get_segm_start(ptr) != idc.BADADDR:
-            yield va, ptr, 4
+        if idc.get_segm_start(ptr) == idc.BADADDR:
+            continue
+
+        yield va, ptr
 
 
 def is_head(va):
@@ -50,24 +50,34 @@ def is_code(va):
         return is_code(head)
 
 
-def is_unknown(va):
-    return ida_bytes.is_unknown(idc.get_full_flags(va))
-
-
 def main():
     for segstart, segend, segname in enum_segments():
-        if segname not in ('.rdata', 'UPX1' ):
-            continue
-
-        for src, dst, psize in find_pointers(segstart, segend):
-            if idc.get_segm_name(dst) not in (".rdata", "UPX0"):
+        for head in idautils.Heads(segstart, segend):
+            if not is_code(head):
                 continue
-                
-            if psize == 8:
-                size = ida_bytes.get_qword(src + 0x8)
-            else:
-                size = ida_bytes.get_dword(src + 0x4)
-                
+
+            # pattern:
+            #
+            #   lea     rax, unk_6BDF88
+            #   mov     [rsp+0], rax
+            #   mov     qword ptr [rsp+8], 40h 
+            if ida_ua.ua_mnem(head) != "lea":
+                continue
+
+            next_head = ida_bytes.next_head(head, idc.BADADDR)
+            if ida_ua.ua_mnem(next_head) != "mov":
+                continue
+
+            next_head2 = ida_bytes.next_head(next_head, idc.BADADDR)
+            if ida_ua.ua_mnem(next_head2) != "mov":
+                continue
+
+            dst = idc.get_operand_value(head, 1)
+            if idc.get_segm_name(dst) not in (".rdata", "UPX1"):
+                continue
+
+            size = idc.get_operand_value(next_head2, 1)
+
             if size > 0x100:
                 continue
             if size <= 2:
@@ -85,22 +95,11 @@ def main():
             except UnicodeDecodeError:
                 continue
 
-            print("string pointer: 0x%x -> 0x%x: %s" % (src, dst, s))
-
-            ida_bytes.del_items(src, 1)
+            print("string pointer: 0x%x -> 0x%x: %s" % (head, dst, s))
+            ida_bytes.del_items(dst, 1)
+            ida_bytes.create_data(dst, idc.FF_BYTE, 1, idc.BADADDR)
             ida_bytes.set_cmt(dst, s, True)
-
-            # pointer
-            ida_bytes.del_items(src, psize)
-            ida_bytes.create_data(src, idc.FF_QWORD if size == 8 else idc.FF_DWORD, psize, idc.BADADDR)
-            # this doesn't seem to always work :-(
-            idc.op_plain_offset(src, -1, 0)
-            ida_name.set_name(src, "s_%x" % (src))
-            ida_bytes.set_cmt(src, s, True)
-
-            # size
-            ida_bytes.del_items(src + psize, psize)
-            ida_bytes.create_data(src + psize, idc.FF_QWORD if size == 8 else idc.FF_DWORD, psize, idc.BADADDR)
+            ida_name.set_name(dst, "s_%x" % (dst))
 
 if __name__ == '__main__':
     main()
